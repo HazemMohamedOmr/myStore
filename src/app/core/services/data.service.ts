2082a76b141ca361d8b625944b6a1ae1f4afd9a4
HazemMohamedOmr <55815459+HazemMohamedOmr@users.noreply.github.com>
2022-08-11 23:14:42 +0200

    Data service logic

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Product } from '../interfaces/product';
 import { HttpClient } from '@angular/common/http';
 
@@ -8,11 +8,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  // products: Observable<Product[]>;
+  private cartProduct: Product[] = [];
+  private _cartProducts = new BehaviorSubject<Array<Product>>([]);
+  cartProducts = this._cartProducts.asObservable();
 
   constructor(private httpClient:HttpClient) { }
 
   getProducts():Observable<Product[]>{
     return this.httpClient.get<Product[]>('/assets/data.json');
+  }
+
+  sendProd(prod:Product):void{
+    this.cartProduct.unshift(prod);
+    this._cartProducts.next(this.cartProduct);
   }
 }
