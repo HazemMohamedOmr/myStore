8e24c514b4ebad22f31a771f2a3422bbe5251a57
HazemMohamedOmr <55815459+HazemMohamedOmr@users.noreply.github.com>
2022-08-11 16:39:21 +0200

    More Dependencies

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
 
 const MaterialComponents: any = [
-  
+  MatButtonModule,
+  MatBadgeModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule
 ]
 
 @NgModule({
