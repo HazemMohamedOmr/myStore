723b1c97ae3238db6f1a35edab0810ae91b54e7a
HazemMohamedOmr <55815459+HazemMohamedOmr@users.noreply.github.com>
2022-08-12 15:47:43 +0200

    Update data.service.ts

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -8,9 +8,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
+  private confirmationBill: object = {}
   private cartProduct: Product[] = [];
+  // private total : number = 0;
+
   private _cartProducts = new BehaviorSubject<Array<Product>>([]);
   cartProducts = this._cartProducts.asObservable();
+
+  private _totalPrice = new BehaviorSubject<number>(0);
+  totalPrice = this._totalPrice.asObservable();
 
   constructor(private httpClient:HttpClient) { }
 
@@ -18,8 +24,67 @@ export class DataService {
     return this.httpClient.get<Product[]>('/assets/data.json');
   }
 
-  sendProd(prod:Product):void{
-    this.cartProduct.unshift(prod);
+  sendProd(prod:Product):string{
+    let message = this.addToCart(prod);
     this._cartProducts.next(this.cartProduct);
+    this.calculatePrice(this.cartProduct);
+    return message;
+  }
+
+  removeProd(prod:Product):string{
+    this.cartProduct = this.cartProduct.filter(v => v.id !== prod.id);
+    console.log(this.cartProduct);
+    
+    this._cartProducts.next(this.cartProduct);
+    this.calculatePrice(this.cartProduct);
+    return `${prod.name} is deleted Successfully!`;
+  }
+
+  resetCartProducts():void{
+    this.cartProduct = [];
+    this._cartProducts.next(this.cartProduct);
+    this.calculatePrice(this.cartProduct);
+  }
+
+  setConfirmBill(bill:object){
+    this.confirmationBill = bill;
+    console.log(this.confirmationBill);
+    
+  }
+
+  get confirmBill(){
+    return this.confirmationBill;
+  }
+
+  private sendPrice(price:number):void{
+    this._totalPrice.next(price);
+  }
+
+  private calculatePrice(products: Product[]):void{
+    let price = 0;
+    products.forEach(ele => {
+      let elePrice = ele.price * ele.quantity;
+      price += elePrice;
+    });
+    this.sendPrice(price);
+  }
+
+  private addToCart(prod:Product) :string{
+
+    for (let index = 0; index < this.cartProduct.length; index++) {
+      const ele = this.cartProduct[index];
+      if(ele.id == prod.id){
+        if(ele.quantity == prod.quantity){
+          return "Product has been Already Added!";
+        }
+        else{
+          ele.quantity = prod.quantity;
+          return "Product Quantity is Updated!";
+        }
+      }
+    }
+
+    this.cartProduct.unshift(prod);
+        return "Product is Added to Cart!";
   }
 }
